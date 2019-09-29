c1d18a72b08bd1fa8b46b5625c57e2233a6a7461
Michael Wlodawsky <michaelwlodawsky@gmail.com>
2019-09-29 14:36:49 -0500

    Initial Commit

---
+++ Dialogue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dialogue
+//
+//  Created by Michael Wlodawsky on 9/29/19.
+//  Copyright © 2019 CS371L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

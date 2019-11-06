093a251779d9ac7a1ea01afff1fd707fa4dfbc25
Michael Wlodawsky <michaelwlodawsky@gmail.com>
2019-11-06 13:22:26 -0600

    put alex's changes back

--- Dialogue/MyDialogueViewController.swift
+++ Dialogue/MyDialogueViewController.swift
@@ -5,34 +5,87 @@
 //  Created by Michael Wlodawsky on 10/22/19.
 //  Copyright © 2019 CS371L. All rights reserved.
 //
-
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 
-class MyDialogueViewController: UIViewController {
+class MyDialogueViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var dialoguesTableView: UITableView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let db = Firestore.firestore()
+    private var userSnapshotListener:ListenerRegistration?
+    private var groups:[String] = []
+    
+    deinit {
+        userSnapshotListener?.remove()
+    }
+    
+    // TODO
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groups.count
+    }
+    
+    // TODO
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: DialogueTableViewCell.identifier, for: indexPath as IndexPath) as! DialogueTableViewCell
+        cell.titleLabel?.text = groups[indexPath.row]
+        cell.subLabel?.text = "temp val"
+        return cell
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Setup listener for user data
+        //        userSnapshotListener = db.collection("users").document(UserHandling.getCurrentUser()!.uid).addSnapshotListener { snapshot, error in
+        //            guard let document = snapshot else {
+        //                print("***ERROR: Error fetching document: \(error!)")
+        //                return
+        //            }
+        //            guard let data = document.data() else {
+        //                print("Document data was empty.")
+        //                return
+        //            }
+        //            print("Current data: \(data)")
+        //            document.documentChanges.forEach { change in
+        //                self.handleDocumentChange(change)
+        //            }
+        //        }
+        //TODO: Remove
+        NetworkHelper.getUserFriendList { (friends, error) in
+            print("\(friends)")
+        }
+        
         // Do any additional setup after loading the view.
         initTableView()
     }
     
     func initTableView() {
-        let rect = CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height))
-        let messageLabel = UILabel(frame: rect)
-        messageLabel.textColor = UIColor.black
-        messageLabel.numberOfLines = 0
-        messageLabel.textAlignment = .center
-        messageLabel.sizeToFit()
-        messageLabel.text = "You don't have any Dialogues yet."
-
-        dialoguesTableView.backgroundView = messageLabel
-        dialoguesTableView.separatorStyle = .none
+        NetworkHelper.getUser(completion: { (user, error) in
+            self.groups = user.groupList
+            if self.groups.count < 1 {
+                let rect = CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height))
+                let messageLabel = UILabel(frame: rect)
+                messageLabel.textColor = UIColor.black
+                messageLabel.numberOfLines = 0
+                messageLabel.textAlignment = .center
+                messageLabel.sizeToFit()
+                messageLabel.text = "You don't have any Dialogues yet."
+                self.dialoguesTableView.backgroundView = messageLabel
+                self.dialoguesTableView.separatorStyle = .none
+            } else {
+                print("HELLO FUCKER")
+                print(self.groups)
+                self.tableView.reloadData()
+            }
+        })
     }
-
+    
     @IBAction func onProfile(_ sender: Any) {
         if let userEmail = UserHandling.getCurrentUserEmail() {
             Alerts.singleChoiceAlert(title: "Login Status", message: "\(userEmail) is logged in.", vc: self)
@@ -42,13 +95,12 @@ class MyDialogueViewController: UIViewController {
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }

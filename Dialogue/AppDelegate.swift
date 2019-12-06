15656b54158429a4b7465385dd0b0c56b8cddbfb
William Lemens <william.lemens@gmail.com>
2019-12-06 12:20:46 -0600

    Cleanup AppDelegate

--- Dialogue/AppDelegate.swift
+++ Dialogue/AppDelegate.swift
@@ -50,7 +50,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
             // instantiate your desired ViewController
             let rootController = storyboard.instantiateViewController(withIdentifier: "DialogueTabBarController")
             
-            // Because self.window is an optional you should check it's value first and assign your rootViewController
+            // Because self.window is an optional you should check its value first and assign your rootViewController
             if let window = self.window {
                 window.rootViewController = rootController
             }
@@ -60,7 +60,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
               withError error: Error!) {
-        if let error = error {
+        if error != nil {
             if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
                 print("The user has not signed in before or they have since signed out.")
             } else {
@@ -71,52 +71,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
         // Perform any operations on signed in user here.
         guard let authentication = user.authentication else { return }
         let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
-                                                          accessToken: authentication.accessToken)
-
-        Auth.auth().signIn(with: credential) { (authResult, error) in
-          if let error = error {
-            // ...
-            return
-          }
-          // User is signed in
-          // ...
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            
-            self.setRootViewController()
-            
-//            let rootVc = self.window?.rootViewController as! UITabBarController
-//            rootVc.performSegue(withIdentifier: "signinToDialogue", sender: nil)
-            
-        }
+                                                       accessToken: authentication.accessToken)
         
-//        let userId = user.userID                  // For client-side use only!
-//        let idToken = user.authentication.idToken // Safe to send to the server
-//        let fullName = user.profile.name
-//        let givenName = user.profile.givenName
-//        let familyName = user.profile.familyName
-//        let email = user.profile.email
-        // ...
-    }
-    
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        Auth.auth().signIn(with: credential) { (authResult, error) in
+            if error != nil {
+                print("sign in error!")
+                return
+            } else {
+                self.setRootViewController()
+            }
+        }
     }
 }

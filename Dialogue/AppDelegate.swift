550f4a7c13b945e444c1f40fd0013ccae9851b50
William Lemens <william.lemens@gmail.com>
2019-12-08 22:38:55 -0800

    Whyy

--- Dialogue/AppDelegate.swift
+++ Dialogue/AppDelegate.swift
@@ -67,18 +67,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
                 print("\(error.localizedDescription)")
             }
             return
-        }
-        // Perform any operations on signed in user here.
-        guard let authentication = user.authentication else { return }
-        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
-                                                       accessToken: authentication.accessToken)
-        
-        Auth.auth().signIn(with: credential) { (authResult, error) in
-            if error != nil {
-                print("sign in error!")
-                return
-            } else {
-                self.setRootViewController()
+        } else {
+            // Perform any operations on signed in user here.
+            guard let authentication = user.authentication else { return }
+            let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                           accessToken: authentication.accessToken)
+            
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                if error != nil {
+                    print("sign in error!")
+                    return
+                } else {
+                    print("USERID: "+(authResult?.user.uid ?? "nil"))
+                    NetworkHelper.getUser(userId: (authResult?.user.uid)!, completion: { (dbuser, error) in
+                        if error != nil || dbuser == nil {
+                            print("writing user... "+(authResult?.user.uid)!)
+                            NetworkHelper.writeUser(user: UserStruct(userId: (authResult?.user.uid)!, displayName: user.profile.name, groupList: [], followList: []), completion: {
+                                print("done")
+                                self.setRootViewController()
+                            })
+                        } else {
+                            self.setRootViewController()
+                        }
+                    })
+                }
             }
         }
     }
